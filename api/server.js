2e41ff3c10c6c8dd7419535e8602dc5f5fb07229
Tien Nguyen <tien.nguyen@asnet.com.vn>
2023-12-28 14:17:11 +0700

    Update server

--- api/server.js
+++ api/server.js
@@ -9,12 +9,12 @@ const path = require('path')
 const filePath = path.join('db.json')
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
-const router = jsonServer.router(db);
+const router = jsonServer.router(db)
 
 // Comment out to allow write operations
 // const router = jsonServer.router('db.json')
 
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
